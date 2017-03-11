b66a73951d55fd509bef3ca1b00ff898c76e8a38
FitiLsan <Fitil_san@mail.ru>
2017-03-11 15:04:23 +0300

    Add files via upload

---
+++ Enemy2.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy2 : MonoBehaviour
+{
+    public Transform Target;
+
+    public float Speed, TimeCast;
+    public float MaxDistance, SpeedRotation, MinDistance, AttackDistance;
+
+    public int Health;
+    public int Damage;
+    Rigidbody MyBody;
+    Transform MyTransform;
+
+    public bool Agressive = false;
+    public bool Couldown = false;
+    public bool IsAlive = true;
+
+    public NavMeshAgent MyAgent;
+
+    // Use this for initialization
+    void Start ()
+    {
+        MyBody = GetComponent<Rigidbody>();
+        MyTransform = transform;
+
+        //Target = GameObject.FindGameObjectWithTag("Player").transform;
+        if (IsAlive && MyAgent.enabled == true)
+            MyAgent.SetDestination(Target.transform.position);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+       
+
+        //Debug.Log("MaxDistance " + MaxDistance);
+        
+
+        if (Vector3.Distance(MyTransform.position, Target.position) < MaxDistance)
+        {
+           // Debug.Log("Дистанция меньше MaxDistance " + MaxDistance);
+            Vector3 rot = Target.position - MyTransform.position;
+            
+            MyTransform.rotation = Quaternion.Slerp(MyTransform.rotation, Quaternion.LookRotation(rot), SpeedRotation * Time.deltaTime);
+
+            if (Vector3.Distance(MyTransform.position, Target.position) > MinDistance)
+                MyTransform.position += MyTransform.forward * Speed * Time.deltaTime;
+
+            if (Vector3.Distance(MyTransform.position, Target.position) < AttackDistance)
+                if (!Couldown)
+                {
+                    Couldown = true;
+                    StartCoroutine(Attack());
+                }
+        }
+        
+    }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(TimeCast);
+
+        if (Vector3.Distance(MyTransform.position, Target.position) < AttackDistance)
+            Target.GetComponent<Player>().TakeDamage(Damage);
+
+        Couldown = false;
+    }
+
+    public void TakeDamage(int Damage, GameObject Agressor)
+    {
+        if (IsAlive)
+        {
+            Health -= Damage;
+
+            // Debug.Log(Agressor);
+
+            if (Health <= 0)
+            {
+                Die();
+            }
+
+            StartCoroutine(BeAgressive(Agressor));
+        }
+    }
+
+    public void Die()
+    {
+        IsAlive = false;
+        Agressive = false;
+        Target = null;
+        MyAgent.SetDestination(transform.position);
+        MyAgent.enabled = false;
+        MyBody.constraints = RigidbodyConstraints.None;
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+
+    IEnumerator BeAgressive(GameObject Agressor)
+    {
+        Agressive = true;
+        Target = Agressor.transform;
+
+        yield return new WaitForSeconds(20);
+
+        Agressive = false;
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Player : MonoBehaviour
+{
+    public GameObject Bullet, StartBullet; // шаблон пули
+    public Rigidbody MyBody;  // Ссылка на свой Rigidboody
+    public float Speed; // Скорость игрока
+    private Vector3 Movement; 
+
+    public bool CanAttack = true, Reload = false;
+
+    public const int Magazine = 30;
+    public int Ammo = 90, CurMagazine = 30;
+
+    public int Health;
+    public float JumpForce;
+
+    public Text DisHealth, DisMagazine, DisAmmo;
+
+    float GroundDis;
+    Collider Col;
+
+    public GameObject Cam;
+    public GameObject Weapon;
+    public float RotSpeedWeapon;
+
+    Ray ray;
+    RaycastHit hit;
+
+    // Use this for initialization
+    void Start()
+    {
+        DisHealth.text = "Health: " + Health;
+        DisAmmo.text = "Ammo: " + Ammo;
+        DisMagazine.text = "Magazine: " + CurMagazine;
+        Col = GetComponent<Collider>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        MyBody = GetComponent<Rigidbody>();
+
+        GroundDis = Col.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            StartCoroutine(Fire());
+
+        if (Input.GetKeyDown(KeyCode.R))
+            StartCoroutine(StartReload());
+
+        ray = new Ray(Cam.transform.position, Cam.transform.forward);
+
+        Physics.Raycast(ray, out hit);
+
+        Vector3 rot;
+
+        if (hit.collider == null)
+            rot = Weapon.transform.forward;
+
+        else
+            rot = hit.point - Weapon.transform.position;
+
+      //  Debug.DrawLine(Cam.transform.position, hit.point, Color.red);
+
+        Weapon.transform.rotation = Quaternion.Slerp(Weapon.transform.rotation, Quaternion.LookRotation(rot), RotSpeedWeapon * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+            MyBody.AddForce(Vector3.up * JumpForce * 20, ForceMode.Impulse);
+
+        float Right = Input.GetAxisRaw("Horizontal");
+        float Forward = Input.GetAxisRaw("Vertical");
+
+        Movement.Set(Forward, 0f, Right);
+
+        MyBody.AddForce(transform.forward * Forward * Speed, ForceMode.Impulse);
+        MyBody.AddForce(transform.right * Right * Speed, ForceMode.Impulse);
+    }
+
+    IEnumerator Fire()
+    {
+        if (CanAttack && CurMagazine > 0 && !Reload)
+        {
+            CanAttack = false;
+
+            CurMagazine--;
+
+            DisMagazine.text = "Magazine: " + CurMagazine;
+
+            GameObject MyBullet = Instantiate(Bullet) as GameObject;
+
+            MyBullet.transform.position = StartBullet.transform.position;
+            MyBullet.transform.rotation = StartBullet.transform.rotation;
+
+            MyBullet.GetComponent<Bullet2>().MyShooter = gameObject;
+
+
+
+            //Instantiate(Bullet, StartBullet.transform.position, StartBullet.transform.rotation);
+
+
+
+            if (CurMagazine <= 0)
+            {
+                StartCoroutine(StartReload());
+                Reload = true;
+            }
+
+            yield return new WaitForSeconds(0.05f);
+
+            CanAttack = true;
+        }
+    }
+
+    IEnumerator StartReload()
+    {        
+        yield return new WaitForSeconds(1f);
+
+        if(Ammo > Magazine)
+        {
+            int Num = Magazine;
+            Num = Num - CurMagazine;
+            Ammo -= Num;
+            CurMagazine = Magazine;
+        }
+
+        else
+        {
+            CurMagazine = Ammo;
+            Ammo = 0;
+        }
+
+        DisAmmo.text = "Ammo: " + Ammo;
+
+        Reload = false;
+    }
+
+    public void TakeDamage(int Damage)
+    {
+        Health -= Damage;
+
+        DisHealth.text = "Health: " + Health;
+
+        if (Health <= 0)
+        {
+            //Условно смерть
+        }
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, GroundDis + 0.1f);
+    }
+}
